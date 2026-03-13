c2ad237aa23dfdfbf94c71d7d22330a4f56c2119
Verónica Vanesa García Zarate <vvanegz@gmail.com>
2026-03-13 12:17:06 -0600

    Refactor Agenda class to use List interface

--- src/Agenda.java
+++ src/Agenda.java
@@ -1,36 +1,42 @@
 import java.util.ArrayList;
+import java.util.List;
 
-public class Agenda {
-    private ArrayList<Contacto> contactos= new ArrayList<>();
-
-    public boolean agregarContacto(Contacto contacto){
-       if(buscarContacto(contacto.getNombre())==null){
-           contactos.add(contacto);
-           return true;
-       }
-       return false;
+class Agenda {
+    private List<Contacto> contactos;
+    public Agenda(){
+        this.contactos = new ArrayList<>();
     }
 
-    public boolean eliminarContacto(Contacto contacto){
-        if(buscarContacto(contacto.getNombre())==null){
-            contactos.remove(contacto);
+    public boolean agregarContacto(Contacto contacto) {
+        if (buscarContacto(contacto.getNombre()) == null) {
+            contactos.add(contacto);
             return true;
         }
         return false;
     }
-    public void mostrarContactos(){
+
+    public boolean eliminarContacto(String nombre) {
+        Contacto contacto= buscarContacto(nombre);
+        if (contacto != null) {
+            return contactos.remove(contacto);
+        }
+        return false;
+    }
+
+    public void mostrarContactos() {
         System.out.println("\n======Todos sus contactos========");
-        if(contactos.isEmpty()){
+        if (contactos.isEmpty()) {
             System.out.println("No hay contactos por mostrar.");
-        } else{
-            for(Contacto contacto: contactos){
-                System.out.println("- "+ contacto);
+        } else {
+            for (Contacto contacto : contactos) {
+                System.out.println("- " + contacto);
             }
         }
     }
-    public Contacto buscarContacto(String nombre){
-        for(Contacto contacto : contactos){
-            if(contacto.getNombre().equalsIgnoreCase(nombre)){
+
+    public Contacto buscarContacto(String nombre) {
+        for (Contacto contacto : contactos) {
+            if (contacto.getNombre().equalsIgnoreCase(nombre)) {
                 return contacto;
             }
         }
